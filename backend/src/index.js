b5e141fa39e30f1525fa913c90aa086d0069da2a
Vinicius Souza <vineasouza33@hotmail.com>
2020-01-16 16:12:10 -0300

    instalando pacote de link back/front

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-wwxlq.mongodb.net/w
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 // json deve vir antes de routes
 app.use(express.json());
 app.use(routes);
