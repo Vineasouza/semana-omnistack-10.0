3500379a463b9981353a4f4f72eee418c1e245fc
Vinicius Souza <vineasouza33@hotmail.com>
2020-01-16 16:11:09 -0300

    finalizando frontend web

--- web/src/App.js
+++ web/src/App.js
@@ -1,95 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
+
 //componente => Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
 //propriedade => Informações que um componente PAI passa para um componente FILHO
 //estado => Informações mantidas pelo componente (Lembrar: imutabilidade)
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect( () => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data]);
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div class="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div class="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-
-            <div class="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/32264094?s=460&v=4" alt="Vinicius Souza"/>
-              <div className="user-info">
-                <strong>Vinicius Souza</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Computer Engineering Student</p>
-            <a href="https://github.com/Vineasouza">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/32264094?s=460&v=4" alt="Vinicius Souza"/>
-              <div className="user-info">
-                <strong>Vinicius Souza</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Computer Engineering Student</p>
-            <a href="https://github.com/Vineasouza">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/32264094?s=460&v=4" alt="Vinicius Souza"/>
-              <div className="user-info">
-                <strong>Vinicius Souza</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Computer Engineering Student</p>
-            <a href="https://github.com/Vineasouza">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars3.githubusercontent.com/u/32264094?s=460&v=4" alt="Vinicius Souza"/>
-              <div className="user-info">
-                <strong>Vinicius Souza</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-            <p>Computer Engineering Student</p>
-            <a href="https://github.com/Vineasouza">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem  key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
