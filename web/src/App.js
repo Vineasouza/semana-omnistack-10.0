6d1eda1b54115886114b8edea648fbd832823c56
Vinicius Souza <vineasouza33@hotmail.com>
2020-01-16 04:32:55 -0300

    add arquivos css, configs e pacotes

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,97 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+//componente => Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+//propriedade => Informações que um componente PAI passa para um componente FILHO
+//estado => Informações mantidas pelo componente (Lembrar: imutabilidade)
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/32264094?s=460&v=4" alt="Vinicius Souza"/>
+              <div className="user-info">
+                <strong>Vinicius Souza</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Computer Engineering Student</p>
+            <a href="https://github.com/Vineasouza">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/32264094?s=460&v=4" alt="Vinicius Souza"/>
+              <div className="user-info">
+                <strong>Vinicius Souza</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Computer Engineering Student</p>
+            <a href="https://github.com/Vineasouza">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/32264094?s=460&v=4" alt="Vinicius Souza"/>
+              <div className="user-info">
+                <strong>Vinicius Souza</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Computer Engineering Student</p>
+            <a href="https://github.com/Vineasouza">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/32264094?s=460&v=4" alt="Vinicius Souza"/>
+              <div className="user-info">
+                <strong>Vinicius Souza</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Computer Engineering Student</p>
+            <a href="https://github.com/Vineasouza">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
